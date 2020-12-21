8eae9c38396d0b395a678cc8a8bfd035a805cb33
ImL1s <aa22396584@gmail.com>
2020-12-21 13:45:16 +0800

    add: search bar lib

--- app/src/main/java/com/iml1s/interview/test/ui/SearchFragment.kt
+++ app/src/main/java/com/iml1s/interview/test/ui/SearchFragment.kt
@@ -1,10 +1,12 @@
 package com.iml1s.interview.test.ui
 
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import com.iml1s.interview.test.databinding.FragmentSearchBinding
+import com.mancj.materialsearchbar.MaterialSearchBar
 
 class SearchFragment : Fragment() {
 
@@ -12,6 +14,21 @@ class SearchFragment : Fragment() {
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
-    ) = FragmentSearchBinding.inflate(inflater).apply { }.root
+    ) = FragmentSearchBinding.inflate(inflater).apply {
+        searchBar.setOnSearchActionListener(object : MaterialSearchBar.OnSearchActionListener {
+            override fun onSearchStateChanged(enabled: Boolean) {
+                Log.d("GG","onSearchStateChanged: $enabled")
+            }
+
+            override fun onSearchConfirmed(text: CharSequence?) {
+                Log.d("GG","onSearchConfirmed: $text")
+            }
+
+            override fun onButtonClicked(buttonCode: Int) {
+                Log.d("GG","onButtonClicked: $buttonCode")
+            }
+        })
+
+    }.root
 
 }
